6af6e78f255607737e856676da3f32db9016bd12
מלכה נבנצל <Hand4c17@mbjcomp.org.il>
2024-03-27 00:03:37 +0200

    Perfect exercise

--- TwitterTower/TwitterTower.cpp
+++ TwitterTower/TwitterTower.cpp
@@ -2,6 +2,7 @@
 //
 
 #include <iostream>
+#include "TwitterTower.h"
 using namespace std;
 
 void rectangularTower(int height, int width)
@@ -34,11 +35,59 @@ void triangleTower(int height, int width)
             cout << "Sorry but the triangle cannot be printed" << endl;
         else
         {
-              
+            printTriangle(width, height);
         }
     }
-
 }
+
+void printLine(int width, int starsCount)
+{
+    int spaceCount = width / 2 - starsCount / 2;
+    for (int i = 0; i < spaceCount; i++)
+        cout << "  ";
+
+    for (int i = 0; i < starsCount; i++)
+        cout << "* ";
+    cout << endl << endl;
+}
+
+void printTriangle(int width, int height)
+{
+    int linesTypes = width / 2 - 1;
+    if (linesTypes == 0 && height > 2)
+        linesTypes = 1;
+    int linesPerType = (height - 2) / linesTypes;
+    int rest = (height - 2) % linesTypes;
+    int starsCount = 1;
+    int countSecondLines = linesPerType + rest;
+
+    printLine(width, starsCount);
+    starsCount += 2;
+
+    printSecondLines(countSecondLines, width, starsCount);
+
+    starsCount += 2;
+
+    for (int i = 0, count = 0; i < height - 2 - countSecondLines; i++)
+    {
+        if (count >= linesPerType) {
+            count = 0;
+            starsCount += 2;
+        }
+        printLine(width, starsCount);
+        count++;
+    }
+    printLine(width, width);
+}
+
+void printSecondLines(int countSecondLines, int width, int starsCount)
+{
+    for (int i = 0; i < countSecondLines; i++)
+    {
+        printLine(width, starsCount);
+    }
+}
+
 
 int main()
 {
@@ -48,7 +97,7 @@ int main()
         cout << "Enter 2 to select a triangle tower tower" << endl;
         cout << "Enter 3 to exit the program" << endl;
         cin >> selected;
-        if (selected != 3)
+        if (selected == 2 || selected == 1)
         {
             cout << "Enter the height and width of the tower" << endl;
             cin >> height >> width;
@@ -58,4 +107,5 @@ int main()
                 triangleTower(height, width);
         }
     } while (selected != 3);
+    return 0;
 }
---
+++ TwitterTower/TwitterTower.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void printSecondLines(int countSecondLines, int width, int starsCount);
+
+void printTriangle(int width, int height);
